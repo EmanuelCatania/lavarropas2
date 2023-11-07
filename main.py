157474557b0295b6b69498eff07a120187e08bc0
Emanuel B. Catania <82476648+EmanuelCatania@users.noreply.github.com>
2023-11-07 13:16:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService 
 import time
 import requests
 
