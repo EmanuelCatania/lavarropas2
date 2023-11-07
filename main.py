404dc929b5a55a9093eeafcafecf036c973d6417
Emanuel B. Catania <82476648+EmanuelCatania@users.noreply.github.com>
2023-11-07 13:36:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService 
+from selenium.webdriver.chrome.service import Service as ChromeService
 import time
 import requests
 
-    # Configurar el navegador
+# Configurar el navegador
 chromedriver_path = '/home/runner/work/lavarropas2/lavarropas2/chromedriver'
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')  # Opcional: Ejecutar Chrome en modo headless (sin interfaz gráfica)
 
+def enviar_notificacion(respuesta):
+    payload = {
+        "text": respuesta
+    }
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    try:
+        response = requests.post(webhook_url, json=payload, headers=headers)
+
+        if response.status_code == 200:
+            print("Notificación enviada con éxito")
+        else:
+            print(f"Error al enviar la notificación. Código de estado: {response.status_code}")
+    except Exception as e:
+        print(f"Error al enviar la notificación: {str(e}")
+
 def main():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    
     # URL de la página web
     url = "https://shop.samsung.com/ar/lavasecarropas-9-5kg-con-control-ia-y-ecobubble-inox---beneficio-pre-registro/p?skuId=137773"
     driver.get(url)
@@ -33,38 +51,12 @@ def main():
         # URL del webhook de Google Chat
         webhook_url = "https://chat.googleapis.com/v1/spaces/AAAARcBFhCU/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=JjX67BMcUAJmQfzfKywG-DhQDphWhULXpoA-sdyi-DU"
 
-        def enviar_notificacion(respuesta):
-            payload = {
-                "text": respuesta
-            }
-            
-            headers = {
-                "Content-Type": "application/json"
-            }
-
-            try:
-                response = requests.post(webhook_url, json=payload, headers=headers)
-
-                if response.status_code == 200:
-                    print("Notificación enviada con éxito")
-                else:
-                    print(f"Error al enviar la notificación. Código de estado: {response.status_code}")
-            except Exception as e:
-                print(f"Error al enviar la notificación: {str(e)}")
-
-        mensaje = respuesta
-        enviar_notificacion(mensaje)
+        enviar_notificacion(respuesta)
 
     except:
         print("Error al buscar el locator.")
         respuesta = "error al buscar el locator"
-        mensaje = respuesta
-        enviar_notificacion(mensaje)
-    
-    # Cerrar el navegador
-    driver.quit()
 
 if __name__ == "__main__":
-    while True:
-        main()
-        time.sleep(3600)  # Esperar 1 hora antes de volver a ejecutar
+    main()
+    time.sleep(3600)
