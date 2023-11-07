a2dd1b01511c20694269f92d2915f1b021078dd6
Emanuel B. Catania <82476648+EmanuelCatania@users.noreply.github.com>
2023-11-07 12:16:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,42 +11,43 @@ url = "https://shop.samsung.com/ar/lavasecarropas-9-5kg-con-control-ia-y-ecobubb
 driver.get(url)
 
 # Locator que deseas validar
-locator = (By.XPATH, "/html/body/div[2]/div/div[1]/div/div/div/div[1]/div/div[7]/div/section/div/div[2]/div/div[4]/div/div/div[2]")
+locator = (By.XPATH, "/html/body/div[2]/div/div/.../div/div[4]/div/div/div[2]")
 
 while True:
     try:
         element = driver.find_element(*locator)
         if element.is_displayed():
             print("Locator encontrado en la página.")
-            respuesta = "no hay stock"
+            respuesta = "no hay stock niconi"
         else:
             print("Locator no encontrado en la página. Notificando...")
-            respuesta = "si hay stock"
+            respuesta = "si hay stock niconi"
+
         # URL del webhook de Google Chat
-webhook_url = "https://chat.googleapis.com/v1/spaces/AAAARcBFhCU/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=JjX67BMcUAJmQfzfKywG-DhQDphWhULXpoA-sdyi-DU"
+        webhook_url = "https://chat.googleapis.com/v1/spaces/AAAARcBFhCU/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=JjX67BMcUAJmQfzfKywG-DhQDphWhULXpoA-sdyi-DU"
 
-def enviar_notificacion(respuesta):
-    payload = {
-        "text": respuesta
-    }
-    
-    headers = {
-        "Content-Type": "application/json"
-    }
+        def enviar_notificacion(respuesta):
+            payload = {
+                "text": respuesta
+            }
+            
+            headers = {
+                "Content-Type": "application/json"
+            }
 
-    try:
-        response = requests.post(webhook_url, json=payload, headers=headers)
+            try:
+                response = requests.post(webhook_url, json=payload, headers=headers)
 
-        if response.status_code == 200:
-            print("Notificación enviada con éxito")
-        else:
-            print(f"Error al enviar la notificación. Código de estado: {response.status_code}")
-    except Exception as e:
-        print(f"Error al enviar la notificación: {str(e)}")
+                if response.status_code == 200:
+                    print("Notificación enviada con éxito")
+                else:
+                    print(f"Error al enviar la notificación. Código de estado: {response.status_code}")
+            except Exception as e:
+                print(f"Error al enviar la notificación: {str(e)}")
 
-# Luego, puedes llamar a la función enviar_notificacion con el mensaje deseado
-mensaje = "Nico el lavarropas!."
-enviar_notificacion(mensaje)
+        mensaje = "Nico el lavarropas!."
+        enviar_notificacion(mensaje)
+
     except:
         print("Error al buscar el locator.")
         respuesta = "error al buscar el locator"
